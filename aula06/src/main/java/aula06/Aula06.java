1ad6edf932f298b089eb4d06a42584dc38081a35
leandro509 <leandrogabriel509@gmail.com>
2025-03-20 16:24:39 -0300

    aula 06

---
+++ aula06/src/main/java/aula06/Aula06.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package aula06;
+
+/**
+ *
+ * @author leandro
+ */
+public class Aula06 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        
+        c.ligar();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+    }
+}
